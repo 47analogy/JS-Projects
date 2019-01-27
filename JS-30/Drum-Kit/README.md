7d8360768ae6f68c3809f5a5aa5a772c8c6d3675
analogy47 <actweed47@gmail.com>
2019-01-27 08:45:55 -0500

    add some comments and README

---
+++ JS-30/Drum-Kit/README.md
@@ -0,0 +1,15 @@
+[JavaScript30](https://javascript30.com/) by [Wes Bos](https://github.com/wesbos)
+
+### Add external source to JavaScript file
+
+### Play sounds for the various keys
+
+    - Add event listener for key down
+    - playKey()
+    - Use data attribute to connect key being pressed to specific sound file (data-xxx)
+
+### Use transition and transform properties in CSS to show which key is being pressed by toggling styles
+
+    - Create reference to keys (nodeList)
+    - Add transitioned event listener to nodeList
+    - removeTransition()
--- JS-30/Drum-Kit/index.js
+++ JS-30/Drum-Kit/index.js
@@ -1,18 +1,19 @@
 const keys = document.querySelectorAll('.key');
 
+// uses key code to play a sound
 const playKey = e => {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-	//console.log(audio);
 	if (audio) {
-		audio.currentTime = 0;
+		audio.currentTime = 0; // prevents delay when playing sound
 		audio.play();
-		key.classList.add('playing');
+		key.classList.add('playing'); // adds playing class when key is pressed
 	} else {
 		return;
 	}
 };
 
+// removes the playing class
 const removeTransition = e => {
 	if (e.propertyName !== 'transform') {
 		return;
@@ -21,5 +22,7 @@ const removeTransition = e => {
 	}
 };
 
+// listen for key press event in the window
 window.addEventListener('keydown', playKey);
+// listen for when CSS transition is completed
 keys.forEach(key => key.addEventListener('transitioned', removeTransition));
