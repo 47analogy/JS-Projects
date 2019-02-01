9151501630deabe9627ccfda0274191dc5f133a4
analogy47 <actweed47@gmail.com>
2019-02-01 17:46:59 -0500

    add horizontal ball movement

--- pong/index.js
+++ pong/index.js
@@ -3,26 +3,45 @@ const canvas = document.querySelector('#game-canvas');
 const ctx = canvas.getContext('2d');
 
 // x-position of ball
-let ballX = 170;
+let ballX = 50;
 
+// x-speed of ball (movement)
+let ballSpeedX = 15;
+
+// variable to set frame rate
+const framesPerSecond = 30;
+
+// draw initial playing screen
 const drawScreen = () => {
-	ballX = ballX + 10;
-
 	// playing field
 	ctx.fillStyle = 'black';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 	// ball
 	ctx.fillStyle = 'white';
-	ctx.fillRect(ballX, 120, 20, 20);
+	ctx.fillRect(ballX, 120, 15, 15);
 
-	// paddles
-
+	// left paddle
 	ctx.fillStyle = 'white';
 	ctx.fillRect(50, 40, 15, 100);
-
+	// right paddle
 	ctx.fillStyle = 'white';
 	ctx.fillRect(750, 150, 15, 100);
 };
 
-drawScreen();
+// move the ball and change direction
+const moveball = () => {
+	ballX = ballX + ballSpeedX;
+	// (- flips the direction)
+	if (ballX >= canvas.width) ballSpeedX = -ballSpeedX;
+	if (ballX <= 0) ballSpeedX = -ballSpeedX;
+};
+
+// update playing screen
+const updateScreen = () => {
+	setInterval(drawScreen, 1000 / framesPerSecond);
+	setInterval(moveball, 1500 / framesPerSecond);
+};
+
+// function calls
+updateScreen();
