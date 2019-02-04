780acaf6a4247592688d02b5e5617aeae744f8a2
analogy47 <actweed47@gmail.com>
2019-02-04 01:21:02 -0500

    add functionality for right paddle movement keep paddles from moving off-screen

--- pong/index.js
+++ pong/index.js
@@ -2,21 +2,29 @@
 const canvas = document.querySelector('#game-canvas');
 const ctx = canvas.getContext('2d');
 
-let ballX = 50; // x-position of ball
-let ballY = 50; // y-position of ball
+// globals
 
-// x-speed/direction of ball (movement)
+// TODO: CENTER BALL ON SCREEN
+let ballX = 0; // x-position of ball
+let ballY = 0; // y-position of ball
+
+// speed/direction of ball (movement)
 let ballDirectionX = 15;
 let ballDirectionY = 15;
 
-// initial y-position of paddle
+// initial positions of paddle
 let player1 = 250;
+let player2 = 250;
 
+const _paddleHeight = 100;
 // y-speed/direction of paddle (movement)
 let player1DirectionY = 20;
+let player2DirectionY = 20;
 
 // variable to set frame rate
 const framesPerSecond = 30;
+
+// end globals
 
 // draw initial playing screen
 const drawScreen = () => {
@@ -28,11 +36,10 @@ const drawScreen = () => {
 	makeBall(ballX, ballY, 7);
 
 	// left paddle
-	leftPaddle(20, player1, 15, 100);
+	leftPaddle(10, player1, 15, _paddleHeight);
 
 	// right paddle
-	// ctx.fillStyle = 'white';
-	// ctx.fillRect(780, 150, 15, player2Paddle);
+	rightPaddle(780, player2, 15, _paddleHeight);
 };
 
 // update playing screen
@@ -44,6 +51,12 @@ const updateScreen = () => {
 // create left paddle
 // paddleY - important param to control up and down movement
 const leftPaddle = (paddleX, paddleY, paddleWidth, paddleHeight) => {
+	ctx.fillStyle = 'white';
+	ctx.fillRect(paddleX, paddleY, paddleWidth, paddleHeight);
+};
+
+// create right paddle
+const rightPaddle = (paddleX, paddleY, paddleWidth, paddleHeight) => {
 	ctx.fillStyle = 'white';
 	ctx.fillRect(paddleX, paddleY, paddleWidth, paddleHeight);
 };
@@ -68,23 +81,35 @@ const moveball = () => {
 	if (ballY <= 0) ballDirectionY = -ballDirectionY;
 };
 
-// move left paddle by updating player1
-// A and Z control player 1 movement
+/* move paddles by updating player1 and player2
+ * A and Z control player 1 / left paddle movement
+ * K and M control player 2 / right paddle movement
+ */
+const PaddleCtrl = e => {
+	const moveUp1 = 65; // A key code
+	const moveDown1 = 90; // Z key code
 
-// TODO: keep paddle from moving off playing surface
-const leftPaddleCtrl = e => {
-	const moveUp = 65;
-	const moveDown = 90;
+	const moveUp2 = 75; // K key code
+	const moveDown2 = 77; // M key code
 
-	if (e.keyCode === moveUp) {
-		player1 = player1 - player1DirectionY;
-	}
-	if (e.keyCode === moveDown) {
-		player1 = player1 + player1DirectionY;
-	}
+	if (e.keyCode === moveUp1) player1 = player1 - player1DirectionY;
+	if (e.keyCode === moveDown1) player1 = player1 + player1DirectionY;
+
+	if (e.keyCode === moveUp2) player2 = player2 - player2DirectionY;
+	if (e.keyCode === moveDown2) player2 = player2 + player2DirectionY;
+
+	// keep paddle from moving off screen
+	if (player1 < 0) player1 = 0;
+	if (player1 > canvas.height - _paddleHeight)
+		player1 = canvas.height - _paddleHeight;
+
+	if (player2 < 0) player2 = 0;
+	if (player2 > canvas.height - _paddleHeight)
+		player2 = canvas.height - _paddleHeight;
 };
 
 // event listeners
-document.addEventListener('keydown', leftPaddleCtrl);
+document.addEventListener('keydown', PaddleCtrl);
+
 // function calls
 updateScreen();
