4fb2a7a6eafbea8f2607b75bd3039e8a1ee1ac4b
analogy47 <actweed47@gmail.com>
2020-07-18 16:57:15 -0400

    display data in UI

--- aws/amplify-js-app/src/app.js
+++ aws/amplify-js-app/src/app.js
@@ -2,8 +2,10 @@
 import Amplify, { API, graphqlOperation } from '@aws-amplify/api'
 import awsconfig from './aws-exports'
 import { createTodo } from './graphql/mutations'
+import {listTodos} from './graphql/queries'
 
 Amplify.configure(awsconfig)
+
 
 
 async function createNewTodo() {
@@ -14,11 +16,22 @@ async function createNewTodo() {
     return await API.graphql(graphqlOperation(createTodo, {input: todo}))
 }
 
+async function getData() {
+    API.graphql(graphqlOperation(listTodos)).then((event) => {
+        event.data.listTodos.items.map((todo, i) => {
+            QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`
+        })
+    })
+}
+
 const MutationButton = document.querySelector('#MutationEventButton')
 const MutationResult = document.querySelector('#MutationResult')
+const QueryResult = document.querySelector('#QueryResult')
 
 MutationButton.addEventListener('click', (event) => {
     createNewTodo().then((event) => {
     MutationResult.innerHTML += `<p>${event.data.createTodo.name} - ${event.data.createTodo.description}</p>`
     })
 })
+
+getData()
