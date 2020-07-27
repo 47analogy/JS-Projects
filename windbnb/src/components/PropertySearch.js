bc176c638df0fbe08a5c06433a07d573f481c4e2
analogy47 <actweed47@gmail.com>
2020-07-27 18:09:13 -0400

    add modal to search component

--- windbnb/src/components/PropertySearch.js
+++ windbnb/src/components/PropertySearch.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+    content : {
+        bottom: '50%',
+    }
+  };
 
 const PropertySearch = () => {
+    const [showModal, setShowModal] = useState(false)
+
+    const handleOpenModal = () => {
+        setShowModal(true)
+    }
+
+    const handleCloseModal = () => {
+        setShowModal(false)
+    }
+
     return ( 
-        <div>Search</div>
+        <div>
+        <button onClick={handleOpenModal}>Trigger Modal</button>
+        <Modal 
+           isOpen={showModal}
+           style={customStyles}
+            >  
+          <button onClick={handleCloseModal}>Close Modal</button>
+          <div>Filter location</div>
+          <div>Search # of guests allowed</div>
+        </Modal>
+      </div>
      );
 }
  
 export default PropertySearch;
+
+/*
+need dropdown filter that displays all locations (city/country)
+need search box for # of guests
+when user click on number of guests (display all properties that fit the criteria)
+*/
